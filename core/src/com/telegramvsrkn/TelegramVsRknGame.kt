45e6c7d87d320e97f3b423492320a4ac12519c30
Kirill Kochnev <kirkocha@gmail.com>
2018-06-23 14:01:22 +0300

    add sound for mainscreen

--- core/src/com/telegramvsrkn/TelegramVsRknGame.kt
+++ core/src/com/telegramvsrkn/TelegramVsRknGame.kt
@@ -1,11 +1,13 @@
 package com.telegramvsrkn
 
 import com.badlogic.gdx.Game
+import com.telegramvsrkn.screens.MainScreen
 
 class TelegramVsRknGame : Game() {
 
 
     override fun create() {
+
         setScreen(MainScreen(this))
     }
 
